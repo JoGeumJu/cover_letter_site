04acf18c4d64b946f9c114327026c0a9b69c688c
JoGeumJu <ccho8702@naver.com>
2024-03-09 16:05:17 +0900

    feat. Gltf 컴포넌트화 #1 - heongee_test2.glb 모델링 컴포넌트화 완료

--- src/pages/mainPage/Box.tsx
+++ src/pages/mainPage/Box.tsx
@@ -1,32 +1,47 @@
 import * as THREE from "three";
-import { useRef, useState } from "react";
-import { useFrame, ThreeElements } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import { useFrame, ThreeElements, useThree } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
 
 type BoxProps = ThreeElements["mesh"];
 
 const Box: React.FC<BoxProps> = (props) => {
   const meshRef = useRef<THREE.Mesh>(null!);
+  const gltf = useGLTF("/assets/models/heongee_test2.glb");
+  const { camera, raycaster, mouse } = useThree();
   //   const [hovered, setHover] = useState<boolean>(false);
   //   const [active, setActive] = useState<boolean>(false);
+  useEffect(() => {
+    // GLTF 모델이 로드된 후에 특정 메쉬를 찾아 처리합니다.
+    const findAndApplyMaterial = (object: THREE.Object3D) => {
+      if (object instanceof THREE.Mesh && object.name === "glass") {
+        // "glass" 메쉬를 찾습니다.
+        const material = object.material as THREE.MeshStandardMaterial; // 혹은 MeshBasicMaterial에 따라 적절히 변경
+        material.transparent = true; // 머티리얼을 투명하게 설정합니다.
+        material.opacity = 0.3;
+      }
+      // 자식 요소에 대해서도 동일한 처리를 재귀적으로 적용합니다.
+      object.children.forEach((child) => {
+        findAndApplyMaterial(child);
+      });
+    };
 
+    // 모델의 Root 객체에서 시작하여 모든 자식 객체를 검색합니다.
+    gltf.scene.children.forEach((child) => {
+      findAndApplyMaterial(child);
+    });
+  }, [gltf]);
   useFrame((state, delta) => {
     if (meshRef.current) {
-      meshRef.current.rotation.x += delta;
+      meshRef.current.rotation.y += delta / 10;
+      meshRef.current.rotation.z = Math.cos(state.clock.elapsedTime) * 0.1;
     }
   });
 
   return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={1}
-      //   onClick={(event) => setActive(!active)}
-      //   onPointerOver={(event) => setHover(true)}
-      //   onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial />
-    </mesh>
+    <>
+      <primitive ref={meshRef} object={gltf.scene} scale={[0.5, 0.5, 0.5]} />
+    </>
   );
 };
 
--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,10 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import Box from "./Box";
+import { Stats, OrbitControls } from "@react-three/drei";
 
 const MainPage: React.FunctionComponent = () => {
   return (
-    <Canvas style={{ width: "100%", height: "100%" }}>
-      <ambientLight intensity={Math.PI / 2} />
+    <Canvas style={{ width: "100%", height: "100%", background: "black" }}>
+      <ambientLight intensity={Math.PI / 2} />1
       <spotLight
         position={[10, 10, 10]}
         angle={0.15}
@@ -13,8 +14,9 @@ const MainPage: React.FunctionComponent = () => {
         intensity={Math.PI}
       />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box />
+      <OrbitControls />
+      <Stats />
     </Canvas>
   );
 };
