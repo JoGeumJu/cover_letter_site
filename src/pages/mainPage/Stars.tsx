e850a13e9ad196cbdf3e5e3d0976e0a2b1f7f0c9
JoGeumJu <ccho8702@naver.com>
2024-03-10 23:57:38 +0900

    feat. Stars생성 #1 - LightController 생성, Light요소들 그룹화 및 컴포넌트화 - Stars 각각 그림자적용(최적화 이슈 고려사항)

--- src/pages/mainPage/Stars.tsx
+++ src/pages/mainPage/Stars.tsx
@@ -7,7 +7,7 @@ import { Mesh } from "three";
 const tempBoxes = new THREE.Object3D();
 
 const Stars: React.FC = () => {
-  const { nodes } = useGLTF("/assets/models/star.glb");
+  const { nodes, materials } = useGLTF("/assets/models/star.glb");
   const ref = useRef<THREE.InstancedMesh>(null!);
   useEffect(() => {
     let counter = 0;
@@ -15,6 +15,7 @@ const Stars: React.FC = () => {
       for (let z = 0; z < 100; z++) {
         const id = counter++;
         tempBoxes.scale.set(0.1, 0.1, 0.1);
+        tempBoxes.castShadow = true;
         tempBoxes.position.set(
           (Math.random() - 0.5) * 200,
           (Math.random() - 0.5) * 200,
@@ -27,32 +28,24 @@ const Stars: React.FC = () => {
   }, []);
 
   useFrame((state, delta) => {
-    // let counter = 0;
-    // const t = clock.oldTime * 0.001;
-    // for (let x = 0; x < 100; x++) {
-    //   for (let z = 0; z < 100; z++) {
-    //     const id = counter++;
-    //     tempBoxes.scale.set(0.1, 0.1, 0.1);
-    //     tempBoxes.position.set(
-    //       (Math.random() - 0.5) * 100,
-    //       (Math.random() - 0.5) * 100,
-    //       (Math.random() - 0.5) * 100
-    //     );
-    //     tempBoxes.rotation.y = t;
-    //     tempBoxes.updateMatrix();
-    //     ref.current.setMatrixAt(id, tempBoxes.matrix);
-    //   }
-    // }
-
     if (ref.current) {
-      ref.current.rotation.y += delta / 30;
+      ref.current.rotation.y += delta / 100;
+      ref.current.rotation.x += delta / 100;
     }
   });
+
+  console.log(materials);
+  console.log(nodes);
 
   return (
     <instancedMesh
       ref={ref}
-      args={[(nodes.Icosphere as Mesh).geometry, undefined, 1000]}
+      // args={[(nodes.Icosphere as Mesh).geometry, undefined, 1000]}
+      args={[
+        (nodes.Icosphere as Mesh).geometry,
+        (nodes.Icosphere as Mesh).material,
+        1000,
+      ]}
     />
   );
 };
--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,24 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import Dog from "./Dog";
 import { Stats, OrbitControls } from "@react-three/drei";
-import { useEffect } from "react";
 import Stars from "./Stars";
+import LightController from "./LightController";
 
 const MainPage: React.FunctionComponent = () => {
   return (
     <Canvas
       style={{ width: "100%", height: "100%", background: "black" }}
       camera={{ position: [0, 0, 6] }}
+      shadows
     >
-      <ambientLight intensity={Math.PI / 2} />1
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <LightController />
       <Dog />
       <Stars />
       <OrbitControls
