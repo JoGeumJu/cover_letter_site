d32ed24f8e75b3903da8724d1672e09280e50360
JoGeumJu <ccho8702@naver.com>
2024-03-31 00:42:20 +0900

    feat. WideBook #13 - DetailPageData 추가 - main사진들 추가

---
+++ src/pages/detailPage/components/PlanetPicture.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+export const PlanetPicture: React.FunctionComponent<{
+  date?: string;
+  name_s?: string;
+  name?: string;
+}> = ({ date, name_s, name }) => {
+  return (
+    <>
+      <DateText>{date}</DateText>
+      <PlanetImageBox>
+        <img
+          src={`/assets/images/move_buttons/${name_s}.png`}
+          style={{ objectFit: "cover" }}
+        />
+      </PlanetImageBox>
+      <ScreenImageBox>
+        <img
+          src={`/assets/images/web_main/${name_s}.png`}
+          style={{
+            width: "88%",
+            height: "82%",
+            opacity: 0.88,
+            borderRadius: "1vh",
+          }}
+        />
+      </ScreenImageBox>
+      <NameTagBox>{name}</NameTagBox>
+    </>
+  );
+};
+
+const NameTagBox = styled.div`
+  display: flex;
+  position: absolute;
+  background-image: url("/assets/images/wide_book/wide_name_tag.png");
+  height: 15%;
+  aspect-ratio: 3.365;
+  background-size: cover;
+  align-items: center;
+  justify-content: center;
+  color: #000000d8;
+  top: 50%;
+  left: 50%;
+  transform: translate(-113%, -265%) rotate(-8deg);
+  font-size: 2.2vh;
+`;
+const PlanetImageBox = styled.div`
+  display: flex;
+  position: absolute;
+  height: 28%;
+  aspect-ratio: 1;
+  top: 32vh;
+  left: 42vh;
+  transform: translate(-50%, -50%) rotate(-1deg);
+`;
+const ScreenImageBox = styled.div`
+  display: flex;
+  position: absolute;
+  height: 15%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-122%, -100%) rotate(-20deg);
+  background-image: url("/assets/images/wide_book/wide_screen_sticker.png");
+  background-size: cover;
+  aspect-ratio: 1.894;
+  align-items: center;
+  justify-content: center;
+`;
+const DateText = styled.div`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  height: 1.7%;
+  aspect-ratio: 13.08;
+  transform: translate(-124%, 310%);
+  color: #c9c8bb;
+  font-size: 1.6vh;
+`;
--- src/pages/detailPage/index.tsx
+++ src/pages/detailPage/index.tsx
@@ -1,17 +1,42 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { styled } from "styled-components";
-import { LoadingRouteButton } from "../../common/LoadingRouteButton";
+import { ContentType, DetailPageData } from "../../data/detail_page_data";
 import { isLoadingState } from "../../recoil/globalState";
-import { WideBook } from "./wide/wide_book";
+import { Explain } from "./components/Explain";
+import { PlanetPicture } from "./components/PlanetPicture";
+import { Release } from "./components/Release";
+import { Stack_Performance } from "./components/Stack_Performance";
 
 const DetailPage: React.FunctionComponent = () => {
-  const setIsLoading = useSetRecoilState(isLoadingState);
   const navigate = useNavigate();
+  const setIsLoading = useSetRecoilState(isLoadingState);
+  const location = useLocation();
+  const [content, setContent] = useState<ContentType>();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const contentKey = searchParams.get("content");
+    if (contentKey) setContent(DetailPageData(contentKey));
+  }, []);
 
   return (
     <DetailWrapper className={"page"} id={"detail"}>
-      <WideBook />
+      <DetailWrapperInner>
+        <Book src={"/assets/images/wide_book/book_detail_wide.png"} />
+        <PlanetPicture
+          date={content?.date}
+          name_s={content?.name_s}
+          name={content?.name}
+        />
+        <Explain explain={content?.explain} />
+        <Release hasReleased={content?.hasReleased} link={content?.link} />
+        <Stack_Performance
+          stack={content?.stack}
+          performance={content?.performance}
+        />
+      </DetailWrapperInner>
     </DetailWrapper>
   );
 };
@@ -24,5 +49,18 @@ const DetailWrapper = styled.main`
 `;
 const DetailWrapperInner = styled.section`
   display: flex;
+  position: relative;
   width: 100%;
+  height: 100%;
+  color: white;
+  overflow: hidden;
+  color: #63635cdf;
+`;
+const Book = styled.img`
+  display: flex;
+  position: absolute;
+  height: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
